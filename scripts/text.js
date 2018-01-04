5b4c2f9b14421f775981cdded182abf55e9b083a
fanta77777 <472849306@qq.com>
2018-01-04 20:14:19 +0800

    tijiao2

--- scripts/text.js
+++ scripts/text.js
@@ -43,8 +43,33 @@ $(function(){
     $(".del").css('display', 'none');
     });
 
-    $('btn').click(function(){
+   
+    function time(){
+        var myDate = new Date();
+        var year = myDate.getFullYear();//获取当前年
+        var yue = myDate.getMonth()+1;//获取当前月
+        var date = myDate.getDate();//获取当前日
+        var h = myDate.getHours();//获取当前小时数(0-23)
+        var m = myDate.getMinutes();//获取当前分钟数(0-59)
+        var s = myDate.getSeconds();//获取当前秒 
+        return yue
+    }
+
+    $('.btn').click(function(){
+        var $parent=$('.word');
+        var $p_1=$('.user-name').val();
+        var $p_2=$('user-msg').val();
+        // var $time=$("year+'年'+yue+' 月'+date+' 日'+h+' 时'+m+' 分'+s+' 秒'");
+        var $li=$('<li class="part"></li>');
+        var $src=$(".current").attr('src');
+        var $part_img=$('<img src="" class="part-img">')
+        $part_img.attr('src',$src)
+
+        $parent.append($li);
+        $li.append($part_img);
         
+
+
     })
 
 
