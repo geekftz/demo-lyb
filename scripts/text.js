0c630bfa3c5b03051c55d19d7fc90f9fc6563d27
fanta77777 <472849306@qq.com>
2018-01-04 23:10:49 +0800

    tijiao3

--- scripts/text.js
+++ scripts/text.js
@@ -37,11 +37,7 @@ $(function(){
         }  
     }); 
 
-    $(".part").hover(function() {
-        $(".del").css('display', 'block');
-    }, function() {
-    $(".del").css('display', 'none');
-    });
+    
 
    
     function time(){
@@ -56,24 +52,46 @@ $(function(){
     }
 
     $('.btn').click(function(){
-        var $parent=$('.word');
-        var $p_1=$('.user-name').val();
-        var $p_2=$('user-msg').val();
-        // var $time=$("year+'年'+yue+' 月'+date+' 日'+h+' 时'+m+' 分'+s+' 秒'");
-        var $li=$('<li class="part"></li>');
+        var $parent=$('ul');
         var $src=$(".current").attr('src');
         var $part_img=$('<img src="" class="part-img">')
-        $part_img.attr('src',$src)
-
-        $parent.append($li);
-        $li.append($part_img);
+        var $p_1=$('.user-name').val();
+        var $p_2=$('.user-msg').val();
+        // var $time=$("year+'年'+yue+' 月'+date+' 日'+h+' 时'+m+' 分'+s+' 秒'");
+        var $li=$('<li class="part"></li>');
+        var $p1=$('<p class="user-pt"></p>');
+        var $p2=$('<p class="time"></p>')
+        var $p3=$('<p><a class="del">删除</a></p>');
+        var $spa1=$('<span class="user-nm"></span>');
+        var $spa2=$('<span class="user-ms"></span>');
         
-
+        if($p_1!=""&&$p_2!=""){
+            $part_img.attr('src',$src);
+            $li.append($part_img);
+            $spa1.append($p_1);
+            $spa2.append($p_2);
+            $p1.append($spa1,':',$spa2);
+            $li.append($p1);
+            $li.append($p2);
+            $li.append($p3);
+            $parent.append($li);
+        }else{
+            alert('请填写您的昵称和留言，谢谢')
+        }
+        
 
     })
 
-
+    $('.del').click(function(){
+        $(this).parent().parent().remove();
+    })
     
+
+    $(".part").hover(function() {
+        $(".del").css('display', 'block');
+    }, function() {
+        $(".del").css('display', 'none');
+    });
 
     
 
